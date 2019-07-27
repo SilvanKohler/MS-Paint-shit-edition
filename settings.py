48120f7ff83b5608e0d9b68589b5807bff936af7
SilvanKohler <edu.silvan.kohler@gmail.com>
2019-07-27 23:36:20 +0200

    settings update (they work now)
    Co-Authored-By: mist124 <mist124@users.noreply.github.com>

--- settings.py
+++ settings.py
@@ -5,25 +5,54 @@ def loadSettings():
     with open('settings.json') as settingsfile:
         return json.load(settingsfile)
 settings = loadSettings()
-selected = []
+_edition = settings['insider']['edition']
+_width = settings['WindowSize']['width']
+_height = settings['WindowSize']['height']
+def finish():
+    pass
 window = Tk()
-window.title(f'MS Paint {settings['edition']} edition')
-window.geometry(f'{settings['WindowSize']['width']}x{settings['WindowSize']['height']}')
+window.title(f'MS Paint {_edition} edition')
+window.geometry(f'{_width}x{_height}')
 scrollbar1 = Scrollbar(window)
-einstellungen = Listbox(window, yscrollcommand=scrollbar1.set)
+tree1 = Listbox(window, yscrollcommand=scrollbar1.set)
+scrollbar2 = Scrollbar(window)
+tree2 = Listbox(window, yscrollcommand=scrollbar2.set)
 einstellung = Entry(window)
-def select(evt):
-    global selected
-    row = einstellungen.curselection()
-    if not row == ():
-        print(row[0])
-        selected = [settings[row[0]], row[0]]
-        einstellung.delete(0, END)
-        einstellung.insert(0, settings[list(settings.keys())[row[0]]])
-einstellungen.bind('<<ListboxSelect>>', select)
-scrollbar1.config(command=einstellungen.yview)
-def insert():
+ok = Button(window, text="OK", command=finish)
+tree1.pack(side=LEFT, fill=BOTH)
+scrollbar1.pack(side=LEFT, fill=Y)
+tree2.pack(side=LEFT, fill=BOTH)
+scrollbar2.pack(side=LEFT, fill=Y)
+einstellung.pack(side=LEFT, fill=X)
+ok.pack(side=LEFT)
+selected = None
+def insert1():
+    tree1.delete(0, END)
     row = 0
-    for key, value in settings.iteritems():
-        einstellungen.insert(row, key)
+    for key, value in settings.items():
+        tree1.insert(row, key)
         row += 1
+def insert2(parent):
+    tree2.delete(0, END)
+    row = 0
+    for key, value in settings[parent].items():
+        tree2.insert(row, key)
+        row += 1
+def select1(evt):
+    global selected
+    row = tree1.curselection()
+    if not row == ():
+        einstellung.delete(0, END)
+        selected = [list(settings.keys())[row[0]], row[0]]
+        insert2(list(settings.keys())[row[0]])
+def select2(evt):
+    row = tree2.curselection()
+    if not row == () and selected:
+        einstellung.delete(0, END)
+        einstellung.insert(0, settings[list(settings.keys())[selected[1]]][list(settings[selected[0]].keys())[row[0]]])
+tree1.bind('<<ListboxSelect>>', select1)
+tree2.bind('<<ListboxSelect>>', select2)
+scrollbar1.config(command=tree1.yview)
+scrollbar2.config(command=tree2.yview)
+insert1()
+mainloop()
