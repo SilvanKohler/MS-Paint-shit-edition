94575c20758a8e81f6ec70d2dbb575b4c0fc561e
SilvanKohler <edu.silvan.kohler@gmail.com>
2019-07-28 22:12:01 +0200

    finished

--- quickAccessBar.py
+++ quickAccessBar.py
@@ -4,18 +4,24 @@ import threading
 import subprocess
 
 
-class colorpicker(threading.Thread):
+class colorPicker(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.process = None
 
     def run(self):
         self.process = subprocess.Popen(['py', 'colorpicker.py'])
-
-
-def openpicker():
-    pick = colorpicker()
-    pick.start()
+picker = False
+pick = None
+def Picker():
+    global picker, pick
+    if not picker:
+        picker = True
+        pick = colorPicker()
+        pick.start()
+    else:
+        picker = False
+        pick.process.kill()
 
 
 def loadSettings():
@@ -28,17 +34,18 @@ _edition = settings['insider']['edition']
 _width = settings['WindowSize']['width'] if settings['quickAccessBarWindowSize'][
     'width'] == "windowSize" else settings['quickAccessBarWindowSize']['width']
 _height = settings['quickAccessBarWindowSize']['height']
-colorpickerIcon = PhotoImage(file="icons/colorpicker.png")
+
 window = Tk()
 window.title(f'quick access - MS Paint {_edition} edition')
 window.geometry(f'{_width}x{_height}')
-
+window.resizable(0,0)
 
 class CustomButton:
     def __init__(self, master, icon, command):
-        self.Button = Button(master)
+        self.Button = Button(master, command=command)
         self.Button.config(image=icon, width="10", height="10")
         self.Button.pack(side=LEFT)
 
-
-colorpickerButton = CustomButton(window, colorpickerIcon, )
+colorpickerIcon = PhotoImage(file="icons/colorpicker.png")
+colorpickerButton = CustomButton(window, colorpickerIcon, command=Picker)
+mainloop()
