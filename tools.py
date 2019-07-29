6af58baa3670ccbb973cd7267d4f1d8e5dfd4748
SilvanKohler <edu.silvan.kohler@gmail.com>
2019-07-29 16:45:22 +0200

    formatted

--- tools.py
+++ tools.py
@@ -17,8 +17,6 @@ selected = None
 settingsBrush = defaultSettingsBrush
 
 
-
-
 window = Tk()
 window.title(f'Werkzeuge - MS Paint {_edition} edition')
 window.geometry(f'{_width}x{_height}')
@@ -29,21 +27,22 @@ menu.pack(side=LEFT, fill=BOTH)
 scrollbar.pack(side=LEFT, fill=Y)
 
 
-
-
-
-
 def updateTool(i):
     tool = tools[i]
     if tool == "Bleistift":
         print("tool_pencil")
     elif tool == "Pinsel":
         print(f"tool_brush_settings: {settingsBrush}")
+        
+
+
 def select(evt):
     global selected
     row = menu.curselection()
     if not row == ():
         updateTool(row[0])
+
+
 menu.bind('<<ListboxSelect>>', select)
 scrollbar.config(command=menu.yview)
 row = 0
