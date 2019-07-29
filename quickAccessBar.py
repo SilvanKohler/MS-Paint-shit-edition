f9d90e1680c2115fbb99ac86bb94938ac69e06ca
SilvanKohler <edu.silvan.kohler@gmail.com>
2019-07-29 17:58:32 +0200

    update

--- quickAccessBar.py
+++ quickAccessBar.py
@@ -4,15 +4,33 @@ import threading
 import subprocess
 import os
 
+pick = None
+
+
+class getColor(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            print('colorpicker:')
+
+
 class colorPicker(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.process = None
 
     def run(self):
-        self.process = subprocess.Popen(['py', 'colorpicker.py'])
+        self.process = subprocess.Popen(['py', 'colorpicker.py'], shell=True, stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+
+
+color = getColor()
+color.start
 picker = False
-pick = None
+
+
 def Picker():
     global picker, pick
     if not picker:
@@ -35,18 +53,19 @@ _width = settings['WindowSize']['width'] if settings['quickAccessBarWindowSize']
     'width'] == "windowSize" else settings['quickAccessBarWindowSize']['width']
 _height = settings['quickAccessBarWindowSize']['height']
 _iconResolution = 64 if settings['insider']['allow64pxIcon'] else 32
-icon = PhotoImage(file=os.path.join(sp, f'icon {_iconResolution}px.png')
 window = Tk()
 window.title(f'Schnellzugriff - MS Paint {_edition} edition')
-window.iconbitmap(icon)
+window.wm_iconbitmap(os.path.join(f'icon {_iconResolution}px.ico'))
 window.geometry(f'{_width}x{_height}')
-window.resizable(0,0)
+window.resizable(0, 0)
+
 
 class CustomButton:
     def __init__(self, master, icon, command):
         self.Button = Button(master, command=command)
         self.Button.config(image=icon, width="10", height="10")
         self.Button.pack(side=LEFT)
+
 
 colorpickerIcon = PhotoImage(file="icons/colorpicker.png")
 colorpickerButton = CustomButton(window, colorpickerIcon, command=Picker)
